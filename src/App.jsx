a1bc1bdd550afb8e44edfc93d7939bfd567392fd
kbchip <charlieshaskins@gmail.com>
2026-07-20 16:17:10 -0700

    Create simple nav bar and include it in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import HomePage from './pages/HomePage'
 import BookDetailsPage from './pages/BookDetailsPage'
 import SignInPage from './pages/SignInPage'
 import CreateAccountPage from './pages/CreateAccountPage'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <>
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/books/:id" element={<BookDetailsPage />} />
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,50 @@
+import { NavLink } from 'react-router-dom'
+
+function NavBar() {
+  const linkClass = ({ isActive }) =>
+    `nav-link ${isActive ? 'active fw-semibold' : ''}`
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom px-3">
+      <div className="container-fluid">
+        <NavLink to="/" className="navbar-brand">
+          Book Reviews
+        </NavLink>
+
+        <button 
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#mainNav"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="mainNav">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink to="/" className={linkClass}>
+                Home
+              </NavLink>
+            </li>
+          </ul>
+
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink to="/sign-in" className={linkClass}>
+                Sign In
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/create-account" className={linkClass}>
+                Create Account
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
