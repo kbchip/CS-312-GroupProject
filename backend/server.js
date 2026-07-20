16030ad03d860eb730d7e4be0815303edb04df6a
kbchip <charlieshaskins@gmail.com>
2026-07-20 14:48:23 -0700

    Add backend route for querying reviews on a book

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Session configuration
 app.use(session({
@@ -33,6 +34,18 @@ app.get("/api/books", async (req, res) => {
     try {
         // Query the database table for all books
         const result = await pool.query('SELECT * FROM books');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+// Database route for reviews on a specific book ID
+app.get("/api/reviews/:id", async (req, res) => {
+    try {
+        // Query DB on given book ID
+        const result = await pool.query('SELECT * FROM reviews WHERE book_id = $1', [req.params.id]);
         res.json(result.rows);
     } catch (err) {
         console.error(err.message);
